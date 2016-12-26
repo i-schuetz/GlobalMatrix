3f535806e7bf1ce3a7a9fc5e6a839c86da94bd9d
Ivan Schuetz <ivanschuetz@gmail.com>
2016-12-26 23:51:33 +0100

    Initial Commit

---
+++ GlobalMatrix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GlobalMatrix
+//
+//  Created by Ivan Schuetz on 26/12/2016.
+//  Copyright © 2016 Ivan Schuetz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
